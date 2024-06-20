d15eac10757e4a6851167065881285443fbbd8cb
P. S <piotrek32141@gmail.com>
2024-06-20 21:08:48 +0200

    Added code for managing multiple threads, sorting and making new files

--- src/main/java/ThreadClass.java
+++ src/main/java/ThreadClass.java
@@ -4,29 +4,32 @@ import java.util.Arrays;
 public class ThreadClass extends Thread {
     private ArrayList<Integer> unorderedList;
     private ArrayList<Integer> sortedList;
+    private int threadNumber;
 
-    public ThreadClass(ArrayList<Integer> startingList) {
+    public ThreadClass(ArrayList<Integer> startingList, int threadNumber) {
         this.unorderedList = startingList;
+        this.threadNumber = threadNumber;
     }
 
     @Override
     public void run() {
         sortedList = bubbleSort(unorderedList);
+        System.out.println("- Thread " + threadNumber + " sorted chunk");
     }
 
     private ArrayList<Integer> bubbleSort(ArrayList<Integer> startingList) {
-        int arrayLength = startingList.size();
-        Integer[] array = startingList.toArray(new Integer[0]);
-        for (int i = 0; i < arrayLength - 1; i++) {
-            for (int j = 0; j < arrayLength - i - 1; j++) {
-                if (array[j] > array[j + 1]) {
-                    int temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
+        int listLength = startingList.size();
+        Integer[] values = startingList.toArray(new Integer[0]);
+        for (int i = 0; i < listLength - 1; i++) {
+            for (int j = 0; j < listLength - i - 1; j++) {
+                if (values[j] > values[j + 1]) {
+                    int temp = values[j];
+                    values[j] = values[j + 1];
+                    values[j + 1] = temp;
                 }
             }
         }
-        return new ArrayList<>(Arrays.asList(array));
+        return new ArrayList<>(Arrays.asList(values));
     }
 
     public ArrayList<Integer> getSortedList() {
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,46 +1,52 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class main {
     public static void main(String[] args) throws IOException {
-        generateNumbersFile("numbers0.1k.txt", 100);
-        generateNumbersFile("numbers1k.txt", 1000);
-        generateNumbersFile("numbers10k.txt", 10000);
 
-        System.out.println("Sorting 100 numbers");
-        sorting("numbers0.1k.txt", 1);
-        sorting("numbers0.1k.txt", 2);
-        sorting("numbers0.1k.txt", 3);
-        sorting("numbers0.1k.txt", 4);
+        System.out.println("<<<   Sorting 100 number file   >>>");
+        sorting("numbers0.1k.txt", "sorted0.1k.txt", 1, 100);
+        sorting("numbers0.1k.txt", "sorted0.1k.txt", 2, 100);
+        sorting("numbers0.1k.txt", "sorted0.1k.txt", 3, 100);
+        sorting("numbers0.1k.txt", "sorted0.1k.txt", 4, 100);
 
-        System.out.println("Sorting 1000 numbers");
-        sorting("numbers1k.txt", 1);
-        sorting("numbers1k.txt", 2);
-        sorting("numbers1k.txt", 3);
-        sorting("numbers1k.txt", 4);
+        System.out.println("\n<<<   Sorting 1000 number file   >>>");
+        sorting("numbers1k.txt", "sorted1k.txt", 1, 1000);
+        sorting("numbers1k.txt", "sorted1k.txt", 2, 1000);
+        sorting("numbers1k.txt", "sorted1k.txt", 3, 1000);
+        sorting("numbers1k.txt", "sorted1k.txt", 4, 1000);
 
-        System.out.println("Sorting 10000 numbers");
-        sorting("numbers10k.txt", 1);
-        sorting("numbers10k.txt", 2);
-        sorting("numbers10k.txt", 3);
-        sorting("numbers10k.txt", 4);
+        System.out.println("\n<<<   Sorting 10000 number file   >>>");
+        sorting("numbers10k.txt", "sorted10k.txt", 1, 10000);
+        sorting("numbers10k.txt", "sorted10k.txt", 2, 10000);
+        sorting("numbers10k.txt", "sorted10k.txt", 3, 10000);
+        sorting("numbers10k.txt", "sorted10k.txt", 4, 10000);
+
+        System.out.println("\n<<<   Sorting 20000 number file   >>>");
+        sorting("numbers20k.txt", "sorted20k.txt", 1, 20000);
+        sorting("numbers20k.txt", "sorted20k.txt", 2, 20000);
+        sorting("numbers20k.txt", "sorted20k.txt", 3, 20000);
+        sorting("numbers20k.txt", "sorted20k.txt", 4, 20000);
+
     }
 
-    private static void generateNumbersFile(String filename, int number_count) throws IOException {
+    private static void generateNumbersFile(String filename, int numberCount) throws IOException {
         Random random = new Random();
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
-            for (int i = 0; i < number_count; i++) {
+            for (int i = 0; i < numberCount; i++) {
                 writer.write((random.nextInt(2 * 1000000) - 1000000) + "\n");
             }
         }
     }
 
-    private static void sorting(String filename, int numberOfThreads){
+    private static void sorting(String inputFile, String outputFile, int numberOfThreads, int numberCount) throws IOException {
+        generateNumbersFile(inputFile, numberCount);
         ArrayList<Integer> listFromFile = new ArrayList<>();
-        ArrayList<Integer> sortedList = null;
+        List<ArrayList<Integer>> sortedSublists = new ArrayList<>();
 
-        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
             String linia;
             while ((linia = br.readLine()) != null) {
                 listFromFile.add(Integer.parseInt(linia));
@@ -51,29 +57,70 @@ public class main {
 
         long startTimeNs = System.nanoTime();
 
-        ThreadClass thread = new ThreadClass(listFromFile);
-        thread.start();
+        int chunkSize = (int) Math.ceil((double) listFromFile.size() / numberOfThreads);
+        ThreadClass[] threads = new ThreadClass[numberOfThreads];
 
-        try {
-            thread.join();
-            sortedList = thread.getSortedList();
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        for (int i = 0; i < numberOfThreads; i++) {
+            int start = i * chunkSize;
+            int end = Math.min(start + chunkSize, listFromFile.size());
+            List<Integer> sublist = listFromFile.subList(start, end);
+            threads[i] = new ThreadClass(new ArrayList<>(sublist), i + 1);
+            threads[i].start();
         }
 
-        System.out.println("Posortowana lista: " + sortedList);
+        for (ThreadClass thread : threads) {
+            try {
+                thread.join();
+                sortedSublists.add(thread.getSortedList());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
 
+        ArrayList<Integer> sortedList = mergeSortedLists(sortedSublists);
 
-
-        //Merge list z threadów
-
-        //Wypisanie czasu
         long endTimeNs = System.nanoTime();
-        System.out.println("Time for " + numberOfThreads + " thread(s): " + ((endTimeNs - startTimeNs)/ 1_000.0) + " μs - Microseconds");
+        System.out.println("Time for " + numberOfThreads + " thread(s): " + ((endTimeNs - startTimeNs) / 1_000.0) + " μs - Microseconds\n");
 
-        //Przekazanie posortowanej listy do nowego pliku
+        writeListToFile(sortedList, outputFile);
     }
 
+    private static ArrayList<Integer> mergeSortedLists(List<ArrayList<Integer>> sortedSublists) {
+        ArrayList<Integer> mergedList = new ArrayList<>();
+        int[] indices = new int[sortedSublists.size()];
 
+        boolean done;
+        do {
+            int minIndex = -1;
+            int minValue = Integer.MAX_VALUE;
+            done = true;
+
+            for (int i = 0; i < sortedSublists.size(); i++) {
+                if (indices[i] < sortedSublists.get(i).size()) {
+                    done = false;
+                    if (sortedSublists.get(i).get(indices[i]) < minValue) {
+                        minValue = sortedSublists.get(i).get(indices[i]);
+                        minIndex = i;
+                    }
+                }
+            }
+
+            if (!done) {
+                mergedList.add(minValue);
+                indices[minIndex]++;
+            }
+        } while (!done);
+
+        return mergedList;
+    }
+
+    private static void writeListToFile(ArrayList<Integer> list, String filename) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            for (Integer number : list) {
+                writer.write(number + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
